0efc9b4455510795f52a20aba7946689f8e564d1
FabricioCruzz <fabriciocruzz77@gmail.com>
2022-06-14 01:37:35 -0300

    Subindo Tr Trrabalho2

---
+++ Trabalho2DB2/src/main/java/br/edu/univas/si5/bd2/runners/Consulta.java
@@ -0,0 +1,10 @@
+package br.edu.univas.si5.bd2.runners;
+
+public class Consulta {
+
+	public static void main(String[] args) {
+		// TODO Implementar as consultas
+
+	}
+
+}
---
+++ Trabalho2DB2/src/main/java/br/edu/univas/si5/bd2/runners/Deleta.java
@@ -0,0 +1,10 @@
+package br.edu.univas.si5.bd2.runners;
+
+public class Deleta {
+
+	public static void main(String[] args) {
+		// TODO Implementar o Delete
+
+	}
+
+}
